38b723fb1629f09589a1d777384bac62d6575a31
Patrice Torguet <torguet@gmail.com>
2019-10-30 09:25:32 +0100

    Modif exos sur variables.

--- src/cours/p2variables/P2Variables.java
+++ src/cours/p2variables/P2Variables.java
@@ -11,14 +11,26 @@ public class P2Variables {
 		
 		unEntier = 2; // attention le signe = est utilisé pour faire une affectation
 		unBooleen = false;
+		unBooleen = unEntier == 2; // voilà le signe == utlisé pour comparer
 		unReel = 2.5;
 		
 		// on peut faire des calculs
 		unEntier = unEntier + 1; 
+		// affichons l'entier
+		System.out.println("unEntier vaut "+unEntier);
 		unEntier++; // raccourci identique à ci-dessus
+		System.out.println("unEntier vaut désormais "+unEntier);
 		unEntier+=1; // encore la même chose
+		System.out.println("unEntier vaut maintenant "+unEntier);
+		
+		// la division de deux entiers est une division entière :
+		unEntier = unEntier / 3; // unEntier valait 5 et on obtiendra 1 car le quotient de la division entière vaut 1.
+		System.out.println("unEntier vaut désormais le quotient "+unEntier);
+		// on peut aussi obtenir le reste d'une division entière (ici de 1 par 2) et l'afficher directement :
+		System.out.println("Reste de la division "+ (unEntier % 2)); // il vaut mieux paranthéser pour que ce soit plus clair
 		
 		unReel = unEntier; // fonctionne car un entier est aussi un réel
+		System.out.println("unReel vaut "+unReel);
 		
 		unReel = 5.2; // une valeur avec partie fractionnaire
 		
