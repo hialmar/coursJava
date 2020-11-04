af0b3b5ddc024ef9f4fdbfab20edea5baba825aa
Patrice Torguet <patrice.torguet@irit.fr>
2020-11-04 10:43:01 +0100

    Ajout d'exemple d'enum

---
+++ src/p12enums/JourSemaine.java
@@ -0,0 +1,17 @@
+package p12enums;
+
+public enum JourSemaine {
+	LUNDI, MARDI, MERCREDI, JEUDI, VENDREDI, SAMEDI, DIMANCHE;
+	
+	public void afficheJour() {
+		System.out.println("Le jour est : "+this);
+	}
+	
+	public boolean estSuiviDe(JourSemaine autreJour) {
+		if(this != DIMANCHE) {
+			return this.ordinal()+1 == autreJour.ordinal();
+		} else {
+			return autreJour == LUNDI;
+		}
+	}
+}
---
+++ src/p12enums/Main.java
@@ -0,0 +1,49 @@
+package p12enums;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Déclaration de quelques jours
+		JourSemaine jour = JourSemaine.LUNDI;
+		JourSemaine lendemain = JourSemaine.MARDI;
+		JourSemaine precedent = JourSemaine.DIMANCHE;
+		
+		jour.afficheJour();
+		
+		// On peut les afficher
+		System.out.println("Hier : "+precedent);
+		System.out.println("Aujourd'hui : "+jour);
+		System.out.println("Demain : "+lendemain);
+		
+		System.out.println(jour.estSuiviDe(lendemain));
+		System.out.println(jour.estSuiviDe(precedent));
+		
+		System.out.println(precedent.estSuiviDe(jour));
+		
+		// On peut récupérer leur numéro d'ordre
+		System.out.println(jour.ordinal());
+		
+		// Et donc on peut les comparer :
+		if(jour.ordinal()<lendemain.ordinal()) {
+			System.out.println("Demain est supérieur à aujourd'hui");
+		}
+		// ou comme cela
+		if(jour.compareTo(lendemain)<0) {
+			System.out.println("Demain est supérieur à aujourd'hui");
+		}
+
+		// On peut aussi parcourir l'ensemble des jours :
+		for(JourSemaine j : JourSemaine.values()) {
+			System.out.println(j);
+		}
+		
+		// On peut aussi affecter un jour à partir de la chaine correspondante
+		JourSemaine nouveauJour = JourSemaine.valueOf("MARDI");
+		System.out.println(nouveauJour);
+		
+		// ceci provoque une exception IllegalArgumentException
+		JourSemaine nouveauJourFaux = JourSemaine.valueOf("JOURDI");
+		System.out.println(nouveauJourFaux);
+	}
+
+}
